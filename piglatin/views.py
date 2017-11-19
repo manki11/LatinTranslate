6ada2f4ff3b34bc6f692e569f151f2b703f86ea6
Mankirat Singh <mankirat1998@gmail.com>
2017-11-20 00:06:18 +0530

    feat: post request made from form

--- piglatin/views.py
+++ piglatin/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def translate(request):
+    originalText= request.POST.get('text')
+    return HttpResponse(originalText)
