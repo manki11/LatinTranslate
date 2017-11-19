f505ba559ac2a04c78f9ca3ab2c6d26f53a67432
Mankirat Singh <mankirat1998@gmail.com>
2017-11-20 00:29:39 +0530

    feat: translate page added

--- piglatin/views.py
+++ piglatin/views.py
@@ -5,5 +5,17 @@ def home(request):
     return render(request, 'home.html')
 
 def translate(request):
-    originalText= request.POST.get('text')
-    return HttpResponse(originalText)
+    originalText= request.POST.get('text').lower()
+    translation='';
+
+    for word in originalText.split():
+        if word[0] in ['a','e','i','o','u']:
+            translation += word
+            translation += 'way '
+        else:
+            translation+=word[1:]
+            translation+=word[0]
+            translation+='ay '
+
+
+    return render(request, 'translate.html')
